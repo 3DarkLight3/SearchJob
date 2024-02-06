7c35733bd2e964e85e11e47c51e63db19aa963fa
3DarkLight3 <103851545+3DarkLight3@users.noreply.github.com>
2024-02-06 16:45:10 +0200

    Update settings.py

--- A_SearchJob/A_SearchJob/settings.py
+++ A_SearchJob/A_SearchJob/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^*5_2ttemjv!=%03kf$cilzbh5fk$l2ecvgi_)5kj+w%3@%u#m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
